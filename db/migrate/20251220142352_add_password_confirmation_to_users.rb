f787d4d9f714573b109f6e8bf0bdaecd72d48144
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-12-20 19:48:53 -0300

    First Commit

---
+++ db/migrate/20251220142352_add_password_confirmation_to_users.rb
@@ -0,0 +1,6 @@
+class AddPasswordConfirmationToUsers < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :password_confirmation, :string
+    add_column :users, :current_password, :string
+  end
+end
