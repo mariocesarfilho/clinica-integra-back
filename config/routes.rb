bf7f7268983b59795a201fbe519acf3cb17aa2c2
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-01-17 12:34:39 -0300

    Update registration route in routes.rb to use create action in AuthsController

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [ :show, :update, :create, :destroy ]
-      post "/register", to: "auths#register"
+      post "/register", to: "auths#create"
     end
   end
 end
