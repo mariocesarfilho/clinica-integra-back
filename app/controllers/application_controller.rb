7972567822484a5f529b9da2263d990a36199a13
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-01-29 21:07:00 -0300

    Add authorization user method to ApplicationController for token validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authorization_user
+
+  private
+
+  def authorization_user
+    header = request.headers["Authorization"]
+    token = header.split("").last if header
+
+    decoded = JasonWebToken.decode(token)
+
+    if decoded
+      @current_user = User.find(decoded[:user_id])
+    else
+      render json: { error: "Não Autorizado!" }, status: :unauthorized
+    end
+  end
 end
