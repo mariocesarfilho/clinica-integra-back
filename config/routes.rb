7a048da98fd31bc843ac7b96877db5362c67a217
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-01-31 00:47:53 -0300

    Add user registration route to API v1

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [ :show, :update, :destroy ]
-      post "/register", to: "auths#create"
+      resources :users, only: [ :show, :update, :destroy, :create ]
     end
   end
 end
